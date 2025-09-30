732ae8437f14ebf888c5363632aa152d45a94f8d
jokool2154 <jeanrdz2154@gmail.com>
2025-09-30 09:21:34 -0500

    Add files via upload

---
+++ Chapter03/ConditionalsandOptionals.swift
@@ -0,0 +1,29 @@
+// If statements execute code in curly braces if the condition is true
+let isPictureVisible = true
+
+// if the value is changed to false nothing would be printed
+
+if isPictureVisible {
+    print("Picture is visible")
+}
+
+
+// isRestaurantFound == false returns true, so the print statement is executed
+let isRestaurantFound = false
+// if the value is changed to true nothing will be printed
+
+if isRestaurantFound == false {
+    print("Restaurant was not found")
+}
+
+
+
+// if-else statement. Code after the else keyword is executed if the condition is false
+    let drinkingAgeLimit = 21
+    var customerAge = 19
+// experiment by changing the customer age to a value greater than 21
+if customerAge < drinkingAgeLimit {
+print("Under age limit")
+} else {
+print("Over age limit")
+}
