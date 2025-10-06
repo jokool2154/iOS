dbff650cc9e680545c28e27dad2ff900e011730c
jenny <jeanrdz2154@gmail.com>
2025-10-06 08:59:26 -0500

    Add files via upload

---
+++ Chapter04/Chapter4RangeOperators.swift
@@ -0,0 +1,20 @@
+// Range Operators and Loops
+// Department Store Program
+// Send Discount vouchers to customers between the ages of 18 and 30
+
+// firstNumber...lastNumber
+// closed range operator
+let myRange = 10...20
+
+for Range in 10...20 {
+    print(Range)
+}
+
+print("")
+
+// half open range operator
+let myRange2 = 10..<20 
+
+for Range2 in 10..<20 {
+    print(Range2)
+}
