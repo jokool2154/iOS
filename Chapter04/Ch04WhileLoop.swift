bd7c6f3d4686513b9d2ac105236661abe2678296
jenny <jeanrdz2154@gmail.com>
2025-10-06 09:42:41 -0500

    Add files via upload

---
+++ Chapter04/Ch04WhileLoop.swift
@@ -0,0 +1,15 @@
+//while loop
+
+//while condition == true {
+// code to be executed
+//}
+
+// condition is checked, and if true, the set of statements in the curly braces are executed.
+
+
+var y = 0
+while y < 50 {
+y += 5
+print("y is \(y)")
+}
+
