f33518ede15670eed7b31af63545fb60e4334529
jenny <jeanrdz2154@gmail.com>
2025-10-06 09:04:44 -0500

    Add files via upload

---
+++ Chapter04/SwiftCH0402Loops.swift
@@ -0,0 +1,20 @@
+// Three types of loops
+
+// 1. for-in
+//2. while
+//3. repeat-while
+
+//for item in sequence {
+//    code
+//}
+
+let myRange: ClosedRange<Int> = 10...20
+let myRange2: Range<Int> = 10..<20
+
+//for number in myRange2 {
+//    print(number)
+//}
+
+for number in 0...5{
+    print(number)
+}
